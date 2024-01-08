fb56702fef3e70e9d1292629570c18db65a7ba09
theac <theac@DESKTOP-LEARA2L>
2024-01-08 18:08:06 +0300

    Add project files.

---
+++ HomeWork5/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string startString = Console.ReadLine();
+            string[] stringArray = startString.Split();
+            int index = 0;  
+            int maxLength = 0;
+            for (int i = 0; i < stringArray.Length; i++)
+
+            {
+                if (stringArray[i].Length > maxLength)
+                {
+                    maxLength = stringArray[i].Length;
+                    index = i;
+                }
+
+            }
+            Console.WriteLine( "Самое длинное слово в предложении: {0}", stringArray[index]);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ IsThisPalindrome/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IsThisPalindrome
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           string initialString = Console.ReadLine();
+           string strippedString = initialString.Replace(" ","").ToLower();
+            bool isPalindrome = true;
+            for (int i = 0; i < strippedString.Length/2; i++)
+            {
+                if (strippedString[i] != strippedString[strippedString.Length - 1-i])
+                {
+                    isPalindrome = false;
+                    break;  
+                }
+                 
+            }
+            if (isPalindrome)
+            {
+                Console.WriteLine("Строка является палиндромом.");
+            }
+            else
+            {
+                Console.WriteLine("Строка не является палиндромом.");
+            }
+            Console.ReadKey();  
+        }
+    }
+}
